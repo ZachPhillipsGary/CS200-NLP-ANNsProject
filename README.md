3397db54221e4b417a62f486fec69e9af735e833
Zachary Phillips-Gary <zpg@Zachs-MacBook-Pro.local>
2016-04-25 00:51:21 -0400

    added CFG generator to feedforward classifier

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 1. Open a command prompt and cd into this directory (CS_JIS_Project)
 2. Cd into dependencies -> maserver-1.0.0 and run ./runmaserver
 3. In a new terminal, cd into the project directory and enter "bin/python corrupt.py" (this will take around 4 hours)
-4. next, run "bin/python lstm.py" (this may take some time), the script will display the results in a window
-5. then run "bin/python nbc.py" (this may take some time), the script will display the results in a window
+4. next, run "bin/python feedforwardClassifier.py" (this may take some time), the script will display the results in a window
+5. then run "bin/python nbClassifier.py" (this may take some time), the script will display the results in a window
+6. finally, run bin/python lstmClassifer.py
--- feedforwardClassifier.py
+++ feedforwardClassifier.py
@@ -13,6 +13,9 @@ from pybrain.datasets            import ClassificationDataSet
 from pybrain.utilities           import percentError
 from pybrain.tools.shortcuts     import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.structure 			 import LinearLayer, SigmoidLayer
+from pybrain.structure 			 import FeedForwardNetwork
+from pybrain.structure 			 import FullConnection
 #from pylab 						 import ion, ioff, figure, draw, contourf, clf, show, hold, plot
 from scipy 					     import diag, arange, meshgrid, where
 from numpy.random 				 import multivariate_normal
@@ -46,20 +49,73 @@ NPChunker = ChunkParser(train_sents)
 #echo out results
 print NPChunker.evaluate(test_sents)
 #End of code from https://www.eecis.udel.edu/~trnka/CISC889-11S/
+inputdim = 1
+#Create ClassificationDataSet
+ds = ClassificationDataSet(inputdim, nb_classes=2, class_labels=['Correct','Incorrect'])
+
 
 #At this point, NPChunker has been trained and is ready to go 
 """
 converttoCFG() -- generates CFG for each sentense in the input corpus file
 @param{String} -> file name of plain text
 pre:
-NPChunker trained and initialized 
+NPChunker trained and ClassificationDataSet initialized 
 post:
-ClassificationDataSet populated with sentence CFG trees
+returns NLTK CFG
 """
 def converttoCFG(file):
 	with open(file, 'r') as f:
 		for line in f:
-			tokens = line.split(".")
+			tokens = line.split(".") #break file into sentences
 			for sentence in tokens:
 				tree = NPChunker.parse(nltk.pos_tag(sentence)) 
+				print tree
+				return tree
+
+"""
+preProcess() -- populates SupervisedDataSet
+@param{String} -> file name of plain text
+@param{String} -> category to place set into ('Correct' or 'Incorrect')
+pre:
+NPChunker trained and ClassificationDataSet initialized 
+post:
+ClassificationDataSet filled with CFGs created from file by NPChunker
+"""
+def preProcess(file, type):
+	ds.appendLinked([str(converttoCFG(file))], [type])
+
+
+#train the network on valid data
+#preProcess('word.txt','Correct')
+brownCorpora = nltk.corpus.brown.fileids()
+for corpus in brownCorpora:
+	fo = open(str(corpus), "w")
+	fo.write(" ".join(nltk.corpus.brown.words(corpus)))
+	fo.seek(0)
+	fo.close()
+	preProcess(corpus,0)
+	pass
+#now add the modified brown corpus 
+for corpus in brownCorpora:
+	fo = open(str(corpus), "w")
+	#append "modified" to the corpus file names to get edited versions
+	preProcess(corpus+"modified",'Correct')
+	pass
+#Create the ANN
+n = FeedForwardNetwork()
+#Add layers
+inLayer = LinearLayer(2)
+hiddenLayer = SigmoidLayer(3)
+outLayer = LinearLayer(1)
+n.addInputModule(inLayer)
+n.addModule(hiddenLayer)
+n.addOutputModule(outLayer)
+#connect layers
+in_to_hidden = FullConnection(inLayer, hiddenLayer)
+hidden_to_out = FullConnection(hiddenLayer, outLayer)
+#add layers to network
+n.addConnection(in_to_hidden)
+n.addConnection(hidden_to_out)
+n.sortModules()
+
 			
